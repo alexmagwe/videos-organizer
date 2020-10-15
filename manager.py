167c444976caf0fd24c50ccda8acb63d5a1818a9
kepler <47972920+alexmagwe@users.noreply.github.com>
2020-10-15 23:32:49 +0300

    all videos final destination is found or created

--- manager.py
+++ manager.py
@@ -1,11 +1,11 @@
-from video import Video
-import os,sys,pickle,shutil,sys
+from video import Video,Series,Movie
+import os,sys,pickle,shutil,sys,re
 from gui import Gui
 from fuzzywuzzy import process
 import threading
 class Manager:
     maxtasks=5
-    types={'video':['.avi','.mkv','.mpeg4'],'documents':['pdf','docx','xlsx','txt'],'music':['mp3','m4a']}
+    types={'video':['.avi','.mkv','.mp4','.mpeg4'],'documents':['pdf','docx','xlsx','txt'],'music':['mp3','m4a']}
     def __init__(self,filetype='video'):
         self.filetypes=self.types[filetype]
         self.files=[]
@@ -14,12 +14,15 @@ class Manager:
         self.getVideos()
         self.sortVideos()
         self.get_destinations()
+        self.createTasks()
     
     def stageMovies():
         pass
+        
     def stageSeries():
         pass
-    
+        
+    @staticmethod
     def createNewFolder(path):
         try:
             os.mkdir(path)
@@ -28,24 +31,13 @@ class Manager:
             print(sys.exc_info()[0])
             return False
     
-    def findDestination(self,name):
-        bestmatch=process.extractOne(name,self.seriesfolders) 
-        foldername=bestmatch[0]
-        percentagematch=bestmatch[1]
-        print (name,' matches the folder ',foldername, 'by ',percentagematch,"%\n",)
-        if percentagematch>=90:
-            return os.path.join(self.seriespath,foldername)
-        else:return False
 
-            
-   
     def getSeriesFolders(self):
         folders=[]
         for _,dirs,_ in os.walk(self.seriespath):
             folders.append(dirs)
             break
         self.seriesfolders=folders[0]
-        print(self.seriesfolders)
 
     def setup(self):
         try:
@@ -55,10 +47,12 @@ class Manager:
         except FileNotFoundError:
             self.getSetup()
             self.setup()
+            
     def getSetup(self):
             self.startGui()
             with open('setup.pkl','wb') as f:
                 pickle.dump(self.values,f)
+                
     def startGui(self):
         gui=Gui()
         while True:
@@ -77,8 +71,7 @@ class Manager:
                     if not error:
                         self.values=values
                         break
-        
-        
+                        
     # sort videos into movies and series
     def getVideos(self):
         for root,dirs,files in os.walk(self.downloadfolder):
@@ -86,36 +79,70 @@ class Manager:
                 name,ext=os.path.splitext(file)
                 if ext in self.filetypes:
                     self.files.append(Video(os.path.join(root,file)))
+                    print(name)
         return self.files
-    def sortVideos(self):
-        self.movies=list(filter(lambda video:video.is_a_movie,self.files))
-        self.series=list(filter(lambda video:video.is_a_series,self.files))
-        print('series:',self.series)
         
-    def get_destinations(self):
-        for video in self.files:
-            if video.is_a_movie:
-                self.destination=self.moviespath
-            elif video.is_a_series:            
-                match=Video.find_name_match(video.name)
-                video.series_name=video.name[:match.start()-1]
-                path=self.findDestination(video.series_name)
-                if path:
-                    self.destination=path
+    def sortVideos(self):
+        movies=list(filter(lambda video:video.is_a_movie,self.files))
+        print(movies)
+        self.movies=list(map(lambda movie:Movie(movie.path),movies))
+        series=list(filter(lambda video:video.is_a_series,self.files))
+        self.series=list(map(lambda eps:Series(eps.path),series))
+    
+        
+    def findDestination(self,eps):
+        bestmatch=process.extractOne(eps.name,self.seriesfolders) 
+        foldername=bestmatch[0]
+        percentagematch=bestmatch[1]
+        print (eps.name,' matches the folder ',foldername, 'by ',percentagematch,"%\n",)
+        if percentagematch>=90:
+            path=os.path.join(self.seriespath,foldername,eps.season)
+            if os.path.exists(path):
+                return path
+            else:
+                created=Manager.createNewFolder(path)
+                if created:
+                    return path
                 else:
-                    path=os.path.join(self.seriespath,video.series_name)
-                    if not os.path.exists(path):
-                        created=Manager.createNewFolder(path)
-                    if created:
-                        print(f'created new folder {video.name}')
-                        self.getSeriesFolders()
-                        self.get_destinations()
-                    else:
-                        print('ould not create folder')
-                        return
+                    return False
+                
+        else:
+            path=os.path.join(self.seriespath,foldername)
+            created=Manager.createNewFolder(path)
+            if created:
+                seasonpath=(os.path.join(path,eps.season))
+                result=Manager.createNewFolder(seasonpath)
+                self.seriesfolders.apend(path)
+                if result:
+                    return seasonpath
+        return False
+
+    def get_destinations(self):
+        for movie in self.movies:
+            movie.destination=self.moviespath
+        for eps in self.series:
+            path=self.findDestination(eps)
+            if path:
+                eps.setDestination(path)
+            else:
+                path=os.path.join(self.seriespath,eps.name,eps.season)
+                if not os.path.exists(path):
+                    created=Manager.createNewFolder(path)
+                if created:
+                    eps.setDestination(path)
+                else:
+                    print('could not create folder')
+                    return
                 
     def createTasks(self):
-        pass
+        print(len(self.series),' episodes found')
+        print(len(self.movies),' movies found')
+        
+        for file in self.movies:
+            print(file.name,' : ',file.destination)
+        for eps in self.series:
+            print(eps.name,' : ',eps.destination)
+        
 
     def startTasks(self):
         pass
--- video.py
+++ video.py
@@ -5,35 +5,66 @@ class Video:
     minmovielength=75
     def __init__(self,path):
         self.path=path
-        self.getSize()
-        self.getName()
+        self.size=self.getSize()
+        self.name=self.getName()
     def __repr__(self):
         return f'{self.getName()}'
-    @staticmethod
-    def find_name_match(name):
-        pattern=r'([s|S][0-9][0-9][e|E][0-9][0-9])'
-        match=re.search(pattern,name)
-        return match
-        
     def getName(self):
         if platform.system()=='Windows':
-            self.name=self.path.split('\\')[-1]
-
-            
+            name=self.path.split('\\')[-1]            
         elif platform.system()=='Linux':        
-            self.name=self.path.split('/')[-1]
-        return self.name
+            name=self.path.split('/')[-1]
+        return name
     def getSize(self):
         self.size=os.stat(self.path).st_size//1000000
         return self.size
+        
+    def getIndex(self,name):
+        pattern=r'([s|S][0-9][0-9][e|E][0-9][0-9])'
+        match=re.search(pattern,name)
+        return match
+    
     @property
     def is_a_movie(self):
         if self.size>750:
             return True
         else:return False
+    
     @property
     def is_a_series(self):
-        match=self.find_name_match(self.name)
+        match=self.getIndex(self.name)
         if match:
             return True
         else: return False
+        
+    def setDestination(self,destination):
+        self.destination=destination
+class Movie(Video):
+    def __init__(self,path):
+        super().__init__(path)
+ 
+class Series(Video):
+    def __init__(self,path):
+        super().__init__(path)
+        self.index=self.getIndex(self.name)
+        self.name=self.setName()
+        self.season=self.setSeason()
+        self.episode_number=self.setEpisode()
+    
+        
+    def setName(self):
+        name=self.name[:self.index.start()-1]
+        return name
+        
+    def setSeason(self):
+        season=self.index.group()[:3]
+        return season
+        
+    def setEpisode(self):
+        episode_number=self.index.group()[-2:]
+        return episode_number
+    
+        
+
+        
+        
